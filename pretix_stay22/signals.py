7bc7dfa9bdf1a90e52d2c0339b54827ac75a4681
Raphael Michel <mail@raphaelmichel.de>
2019-10-21 18:26:48 +0200

    Settings

--- pretix_stay22/signals.py
+++ pretix_stay22/signals.py
@@ -1,4 +1,3 @@
-# Register your receivers here
 from datetime import timedelta
 from urllib.parse import urlparse, urlencode
 
@@ -7,15 +6,18 @@ from django.contrib.staticfiles import finders
 from django.dispatch import receiver
 from django.http import HttpRequest, HttpResponse
 from django.template.loader import get_template
+from django.urls import resolve, reverse
+from django.utils.translation import gettext_lazy as _
 
 from pretix.base.middleware import _parse_csp, _merge_csp, _render_csp
 from pretix.base.models import Order, Event
+from pretix.control.signals import nav_event_settings
 from pretix.presale.signals import order_info, process_response, sass_postamble
 
 
 def stay22_params(event, ev, ev_last, order):
     p = {
-        'aid': 'pretix-{}{}'.format(urlparse(settings.SITE_URL).hostname, event.pk),
+        'aid': event.settings.stay22_aid or 'pretix-{}{}'.format(urlparse(settings.SITE_URL).hostname, event.pk),
         'maincolor': event.settings.primary_color[1:],
         'venue': str(ev.location).replace('\n', ', '),
         'ljs': order.locale[:2],
@@ -38,14 +40,20 @@ def stay22_params(event, ev, ev_last, order):
 @receiver(order_info, dispatch_uid="stay22_order_info")
 def order_info(sender: Event, order: Order, **kwargs):
     subevents = {op.subevent for op in order.positions.all()}
-    ctx = {
-        'params': urlencode(stay22_params(
-            sender,
-            min(subevents, key=lambda s: s.date_from) if sender.has_subevents else sender,
-            max(subevents, key=lambda s: s.date_to or s.date_from) if sender.has_subevents else sender,
-            order
-        )),
-    }
+    if sender.settings.stay22_embedlink:
+        ctx = {
+            'url': sender.settings.stay22_embedlink
+        }
+    else:
+        ctx = {
+            'url': 'https://www.stay22.com/embed/gm?{}'.format(urlencode(stay22_params(
+                sender,
+                min(subevents, key=lambda s: s.date_from) if sender.has_subevents else sender,
+                max(subevents, key=lambda s: s.date_to or s.date_from) if sender.has_subevents else sender,
+                order
+            )))
+        }
+
     template = get_template('pretix_stay22/order_info.html')
     return template.render(ctx)
 
@@ -72,3 +80,20 @@ def r_sass_postamble(sender, filename, **kwargs):
         with open(finders.find('pretix_stay22/postamble.scss'), 'r') as fp:
             return fp.read()
     return ""
+
+
+@receiver(nav_event_settings, dispatch_uid='stay22_nav')
+def navbar_info(sender, request, **kwargs):
+    url = resolve(request.path_info)
+    if not request.user.has_event_permission(request.organizer, request.event, 'can_change_event_settings',
+                                             request=request):
+        return []
+    return [{
+        'label': _('Stay22'),
+        'icon': 'house',
+        'url': reverse('plugins:pretix_stay22:settings', kwargs={
+            'event': request.event.slug,
+            'organizer': request.organizer.slug,
+        }),
+        'active': url.namespace == 'plugins:pretix_stay22',
+    }]
